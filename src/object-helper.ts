6f2b2f9e4707306475c4c3947fb0d6a47f408d0a
Gabriel Nobel <gabriel.nobel@abraxas.ch>
2022-03-03 16:30:40 +0100

    added comments for deepCopyObject

--- src/object-helper.ts
+++ src/object-helper.ts
@@ -1,12 +1,12 @@
-export function copy<T>(source: T): T {
+export function copy<T extends object>(source: T): T {
     return { ...source }
 }
 
-export function stringifyAndParse<T>(source: T): T {
+export function stringifyAndParse<T extends object>(source: T): T {
     return JSON.parse(JSON.stringify(source))
 }
 
-export function compareObjectsByString(objectOne: any, objectTwo: any): boolean {
+export function compareObjectsByString<T extends object>(objectOne: T, objectTwo: T): boolean {
     return JSON.stringify(objectOne) === JSON.stringify(objectTwo);
 }
 
@@ -16,6 +16,14 @@ export function compareObjectsAdvanced(objectOne: any, objectTwo: any): boolean 
     return false;
 }
 
+  /**
+   * 
+   * This function is to deep copy an object into another of the same type without changid their references.
+   * 
+   * 
+   * @param source - the source object which is deep copied.
+   * @param dest  - the destination which should get overwritten.
+   */
 export function deepCopyObject<T extends object>(source: T, dest: T) {
     let isSourceObject = typeof source === 'object' && source !== null;
     let isDestObject = typeof dest === 'object' && dest !== null;
